30d73ef7a48009d18827cef902b92f22bdfe7028
Tom Vaughan <tom@software6.net>
2012-01-02 19:35:55 -0300

    this install the twitter-bootstrap version of django-userena

--- puppet/modules/django/manifests/init.pp
+++ puppet/modules/django/manifests/init.pp
@@ -1,37 +1,44 @@
 class django {
-    package {
-        "python-django":
-            ensure => latest,
-            provider => apt;
-    }
-    package {
-        "python-django-south":
-            ensure => latest,
-            provider => apt;
-    }
-    package {
-        "python-django-collector":
-            ensure => latest,
-            provider => apt;
-    }
-    package {
-        "django-tastypie":
-            ensure => latest,
-            provider => pip;
-          # pip install --user ???
-    }
-    package {
-        "python-dateutil":
-            ensure => latest,
-            provider => apt;
-          # precise
-          # required by tastypie
-    }
-    package {
-        "python-mimeparse":
-            ensure => latest,
-            provider => apt;
-          # precise
-          # required by tastypie
-    }
+  package {
+    "python-django":
+      ensure => latest,
+      provider => apt,
+      alias => "Django";
+  }
+  package {
+    "python-django-south":
+      ensure => latest,
+      provider => apt,
+      require => Package["python-django"];
+  }
+  package {
+    "python-django-collector":
+      ensure => latest,
+      provider => apt,
+      require => Package["python-django"];
+  }
+  package {
+    "python-dateutil":
+      ensure => latest,
+      provider => apt;
+    # TODO: precise
+  }
+  package {
+    "python-mimeparse":
+      ensure => latest,
+      provider => apt;
+    # TODO: precise
+  }
+  package {
+    "django-tastypie":
+      ensure => latest,
+      provider => pip,
+      require => Package["python-django", "python-dateutil", "python-mimeparse"];
+  }
+  package {
+    "git+git://github.com/rentalita/django-userena.git@twitter-bootstrap":
+      ensure => latest,
+      provider => pip,
+      require => Package["python-django"];
+  }
 }
