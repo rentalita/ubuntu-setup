8f550f56d64344800b133c08546f0fdb493975da
Tom Vaughan <tom@software6.net>
2012-01-03 16:31:53 -0300

    install python development tools via puppet

--- puppet/modules/python/manifests/dev.pp
+++ puppet/modules/python/manifests/dev.pp
@@ -1,2 +1,22 @@
 class python::dev inherits python {
+  package {
+    "python-doc":
+      ensure => latest,
+      provider => apt;
+  }
+  package {
+    "pep8":
+      ensure => latest,
+      provider => apt;
+  }
+  package {
+    "pylint":
+      ensure => latest,
+      provider => apt;
+  }
+  package {
+    "python-sphinx":
+      ensure => latest,
+      provider => apt;
+  }
 }
