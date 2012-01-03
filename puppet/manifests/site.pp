a802b56f193c4adadb4ede1ba321a443a8f2c3e2
Tom Vaughan <tom@software6.net>
2012-01-03 17:47:10 -0300

    initial attempt to provide files via puppet

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,21 @@
 import "nodes"
 
+file {
+  "/etc/apt/apt.conf.d/99local":
+    owner  => root,
+    group  => root,
+    mode   => 644,
+    source => "puppet:///modules/apt/etc/apt/apt.conf.d/99local";
+}
+
+file {
+  "/etc/apt/preferences.d/99local":
+    owner  => root,
+    group  => root,
+    mode   => 644,
+    source => "puppet:///modules/apt/etc/apt/preferences.d/99local";
+}
+
 package {
   "git":
     ensure => latest,
