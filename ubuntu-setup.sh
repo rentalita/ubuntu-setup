e9b3b9e37b4da058c06fbb8f24ec930d4b04642e
Tom Vaughan <tom@software6.net>
2012-05-27 15:36:58 -0400

    update notes on initial manual steps

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -6,30 +6,35 @@
 # apt-get install --no-install-recommends git
 # apt-get clean
 
+# dpkg-reconfigure tzdata
+
 # adduser tvaughan
 # adduser tvaughan sudo
 # passwd -dl root
+
+# dpkg-reconfigure gitolite
+# openssl passwd -1 T0pS3cR3t
 
 # vi /etc/hostname /etc/hosts
 # vi /etc/default/dhcpcd (maybe)
 # service hostname restart
 
-# dpkg-reconfigure tzdata
-
-# echo "deb http://ppa.launchpad.net/saltstack/salt/ubuntu precise main" > /etc/apt/sources.list.d/saltstack.list
+# echo "deb http://ppa.launchpad.net/saltstack/salt/ubuntu precise main" > \
+#     /etc/apt/sources.list.d/saltstack.list
 # apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0E27C0A6
 # apt-get update
-# apt-get install --no-install-recommends salt-master salt-minion
-# apt-get clean
 
 # on minion:
+#   apt-get install --no-install-recommends salt-minion
 #   set "master" to "appropriate hostname" in /etc/salt/minion
 #   service salt-minion restart
 # on master:
+#   apt-get install --no-install-recommends salt-master
 #   salt-key -A (danger!!!)
 #   git clone git://github.com/rentalita/ubuntu-setup.git
 #   ln -s $(pwd)/ubuntu-setup/salt /srv/salt
 #   ln -s $(pwd)/ubuntu-setup/pillar /srv/pillar
+#   service salt-master restart
 
 sudo salt --no-color -t 6000 '*' state.highstate
 
